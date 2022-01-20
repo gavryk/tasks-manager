866e909e2964de580cf972f76831d1c075be6415
gvozdyk <gvozdyk23@gmail.com>
2022-01-21 01:05:46 +0200

    check done task in all tasks page

--- src/components/TasksGroup/Tasks/Task/Task.jsx
+++ src/components/TasksGroup/Tasks/Task/Task.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import style from "./Task.module.scss";
 import parse from "html-react-parser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import style from "./Task.module.scss";
 
 const Task = ({
   id,
@@ -20,14 +21,20 @@ const Task = ({
   }
 
   return (
-    <div
-      className={`${style.taskCard} ${checkable ? style.checkableTask : ""} ${done ? style.done : ""}`}
-    >
-      {checkable && (
-        <div className={`${style.check}`} onClick={check}>
-          <FontAwesomeIcon icon={faCheck} />
-        </div>
-      )}
+    <div className={`${style.taskCard} ${checkable ? style.checkableTask : ""} ${done ? style.done : ""}`}>
+      {
+        checkable 
+        ? (
+          <div className={`${style.check}`} onClick={check}>
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+        )
+        : (
+          <div className={`${style.status} ${ done ? style.statusDone : style.statusInProcess }`}>
+            <span>{done ? 'Complete' : 'In Process'}</span>
+          </div>
+        )
+      }
       <div className={style.taskTitle}>
         <h2>{title}</h2>
       </div>
