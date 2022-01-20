adde48342938b66a20b99bf5ae4e63c91da465ef
gvozdyk <gvozdyk23@gmail.com>
2022-01-21 01:29:19 +0200

    toggle edit mode for title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import style from "./Header.module.scss";
 
 const Header = ({ handleSidebar, sidebarAct, activeTasks }) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const editTitle = () => {
+    setEditMode(!editMode);
+  }
+
   return (
     <div style={{ backgroundColor: activeTasks.color }} className={`${style.headerWrapper}`}>
       <div
@@ -14,7 +22,10 @@ const Header = ({ handleSidebar, sidebarAct, activeTasks }) => {
           ${style.tasksTitle} 
           ${activeTasks.color === '#ffffff' || !activeTasks.color ? style.white : ''}
         `}>
-        <h1>{activeTasks.title ? activeTasks.title : "All Tasks"}</h1>
+        <h1>
+          {activeTasks.title ? ( !editMode ? activeTasks.title : 'Edit Mode') : "All Tasks"}
+          {activeTasks.title && <button onClick={editTitle} className={style.editBtn}><FontAwesomeIcon icon={faEdit} /></button>}
+        </h1>
       </div>
     </div>
   );
